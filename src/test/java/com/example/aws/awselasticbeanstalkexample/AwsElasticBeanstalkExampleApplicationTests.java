9f7294af6dad87877552facf2e6aa4ecf33dcaab
lukas.andren <lukas.andren@columbusglobal.com>
2019-12-19 17:17:00 +0100

    added testcase testCalculateSum

--- src/test/java/com/example/aws/awselasticbeanstalkexample/AwsElasticBeanstalkExampleApplicationTests.java
+++ src/test/java/com/example/aws/awselasticbeanstalkexample/AwsElasticBeanstalkExampleApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.aws.awselasticbeanstalkexample;
 
+import com.example.aws.awselasticbeanstalkexample.bean.Calculation;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class AwsElasticBeanstalkExampleApplicationTests {
+	private CalculationController calculationController = new CalculationController();
+	private Calculation calculation = new Calculation();
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testCalculateSum(){
+
+		calculation.setNumberOne("10");
+		calculation.setNumberTwo("5");
+
+		calculationController.calculateSum(calculation);
+
+		assert "15".equals(calculation.getSum());
+
 	}
 
 }
